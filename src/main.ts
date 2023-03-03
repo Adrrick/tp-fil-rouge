e0187936e154e5f77a7b49d62a6e1a72c926625b
Adrrick <adrikgw2@gmail.com>
2023-03-03 10:04:13 +0100

    feat: :sparkles: Basic Routing w/ home,login

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,19 @@ import { provideAuth } from "@angular/fire/auth";
 import { provideFirestore } from "@angular/fire/firestore";
 import { provideStorage } from "@angular/fire/storage";
 import { bootstrapApplication } from "@angular/platform-browser";
+import { provideRouter } from "@angular/router";
 import { ServiceWorkerModule } from "@angular/service-worker";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { AppComponent } from "./app/app.component";
+import { appRoutes } from "./app/app.routes";
 import { environment } from "./environments/environment";
 
 bootstrapApplication(AppComponent, {
   providers: [
+    provideRouter(appRoutes),
     importProvidersFrom(
       ServiceWorkerModule.register('ngsw-worker.js', {
         enabled: !isDevMode(),

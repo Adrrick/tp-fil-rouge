c9366457a8d83f6b8036962355b8e9925d67dd44
Adrrick <adrikgw2@gmail.com>
2023-03-03 09:49:20 +0100

    refactor: :recycle: Switched to standalone Angular app

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,28 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { importProvidersFrom, isDevMode } from "@angular/core";
+import { provideFirebaseApp } from "@angular/fire/app";
+import { provideAuth } from "@angular/fire/auth";
+import { provideFirestore } from "@angular/fire/firestore";
+import { provideStorage } from "@angular/fire/storage";
+import { bootstrapApplication } from "@angular/platform-browser";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { AppComponent } from "./app/app.component";
+import { environment } from "./environments/environment";
 
-import { AppModule } from './app/app.module';
-
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(
+      ServiceWorkerModule.register('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000',
+      })
+    ),
+    importProvidersFrom(provideFirebaseApp(() => initializeApp(environment.firebase))),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+    importProvidersFrom(provideStorage(() => getStorage())),
+  ],
+}).catch((err) => console.error(err));

c16d831c02af5e9d217b96a32050fdd0749d4ac1
Adrrick <adrikgw2@gmail.com>
2023-03-03 09:45:41 +0100

    build: :arrow_up: Added Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
@@ -18,6 +23,10 @@ import { ServiceWorkerModule } from '@angular/service-worker';
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
     }),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'tp-fil-rouge-bf29f',
+    appId: '1:7493998023:web:44302a7b5d0adf49e435e2',
+    storageBucket: 'tp-fil-rouge-bf29f.appspot.com',
+    apiKey: 'AIzaSyAjb2SJSC4eka0TQ5iQP-XYl4cVOYdcW7I',
+    authDomain: 'tp-fil-rouge-bf29f.firebaseapp.com',
+    messagingSenderId: '7493998023',
+  },};

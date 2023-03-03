f92a197940d5461c0d6dd1ccf501d8a3bb4f19ed
Hugo Huzé--Cardosi <hugo.cardosi@hotmail.fr>
2023-03-03 12:43:19 +0100

    feat(movie): :sparkles: page for movie detail

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
-import { Route } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
+import {Route} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {LoginComponent} from './login/login.component';
+import {MovieDetailComponent} from "./movies/movie-detail/movie-detail.component";
 
 export const appRoutes: Route[] = [
-  { path: '', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'movie/:id', component: MovieDetailComponent}
 ];
---
+++ src/app/movies/movie-detail/movie-detail.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MoviesService} from "../../shared/services/movies.service";
+import {ActivatedRoute} from '@angular/router';
+import {Observable} from "rxjs";
+
+
+@Component({
+  selector: 'tp-fil-rouge-movie-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './movie-detail.component.html',
+  styleUrls: ['./movie-detail.component.scss'],
+  providers: [MoviesService]
+})
+export class MovieDetailComponent implements OnInit {
+  movie$: Observable<any> | undefined;
+
+  constructor(private moviesServices: MoviesService, private route: ActivatedRoute,) {
+  }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.movie$ = this.moviesServices.getMovie(id);
+    }
+
+  }
+}

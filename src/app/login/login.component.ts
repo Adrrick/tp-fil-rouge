92987b2645fd26d5e5c0cc4308a208b2525ed2f2
Adrrick <adrikgw2@gmail.com>
2023-03-09 09:43:25 +0100

    feat: :sparkles: Base login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'tp-fil-rouge-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+
+}

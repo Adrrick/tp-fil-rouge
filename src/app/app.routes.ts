5ce4afb5753312541f7bb822ff6889ff897d9ce1
Hugo Huzé--Cardosi <hugo.cardosi@hotmail.fr>
2023-03-09 12:18:11 +0100

    feat(auth): :sparkles: login with firebase auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import {Route} from '@angular/router';
 import {HomeComponent} from './home/home.component';
-import {LoginComponent} from './login/login.component';
+import {LoginComponent} from './pages/login/login.component';
 import {MovieDetailComponent} from "./movies/movie-detail/movie-detail.component";
+import {RegisterComponent} from "./pages/register/register.component";
 
 export const appRoutes: Route[] = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
   {path: 'movie/:id', component: MovieDetailComponent}
 ];

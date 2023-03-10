df2da3a571e5bce1c9d9e8dabff9a77e917b30a7
Hugo Huzé--Cardosi <hugo.cardosi@hotmail.fr>
2023-03-10 14:17:58 +0100

    feat(auth-register): :sparkles: detect if username already used

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,6 +1,7 @@
 
 
 export default interface User {
+  uid: string;
   username: string;
   image?: string; // TODO transform to image/blob
   moviesSeen: {movieId: string; posterPath: string;}[];
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {FirebaseAuthService} from "../../shared/services/firebase-auth.service";
 import {MatchPasswordDirective} from "../../shared/validators/match-password.directive";
+import {UserService} from "../../shared/services/user.service";
 
 @Component({
   selector: 'tp-fil-rouge-register',
@@ -16,21 +17,31 @@ export class RegisterComponent {
   registerForm: FormGroup;
 
   constructor(private readonly fb: FormBuilder,
-              private authServices: FirebaseAuthService) {
+              private authServices: FirebaseAuthService,
+              private userServices: UserService,) {
     this.registerForm = fb.group({
-      username: [undefined, [Validators.required]],
-      email: [undefined, [Validators.required, Validators.email]],
-      password: [undefined, [Validators.required]],
-      confirmedPassword: [undefined, [Validators.required]]
+        username: ['', [Validators.required]],
+        email: [undefined, [Validators.required, Validators.email]],
+        password: [undefined, [Validators.required]],
+        confirmedPassword: [undefined, [Validators.required]]
       },
       {
         validators: [new MatchPasswordDirective()]
       });
   }
 
-  submit(): void {
-    this.authServices.signUpEmail(this.registerForm.controls['email']?.value,
-      this.registerForm.controls['password']?.value,
-      this.registerForm.controls['username']?.value);
+  async submit(): Promise<void> {
+    const usernameCondition = await new Promise((resolve) => {
+      this.userServices.getUserByUsername(this.registerForm.controls['username']?.value).subscribe((users) => {
+        resolve(users.length > 0);
+      })
+    });
+    if (!usernameCondition) {
+      this.authServices.signUpEmail(this.registerForm.controls['email']?.value,
+        this.registerForm.controls['password']?.value,
+        this.registerForm.controls['username']?.value);
+    } else {
+      console.log('Username already used');
+    }
   }
 }

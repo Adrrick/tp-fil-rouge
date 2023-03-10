766c5de4bf599c724a620201920580fae220c6ad
Tristan <adrikgw2@gmail.com>
2023-03-10 11:23:15 +0100

    feat: :sparkles: Display usernames on profiles

--- src/app/pages/profiles/profiles.component.ts
+++ src/app/pages/profiles/profiles.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserService } from 'src/app/shared/services/user.service';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'tp-fil-rouge-profiles',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatDividerModule, MatListModule],
   templateUrl: './profiles.component.html',
   styleUrls: ['./profiles.component.scss'],
+  providers: [UserService],
 })
-export class ProfilesComponent {}
+export class ProfilesComponent {
+  constructor(private usersService: UserService) {}
+
+  users$ = this.usersService.getAllUsers();
+}

8876f757a682b9678c49188624b83214330c8a04
Tristan <adrikgw2@gmail.com>
2023-03-10 12:19:02 +0100

    feat: :sparkles: Added Profiles and its routing

---
+++ src/app/pages/profiles/profiles.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'tp-fil-rouge-profiles',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './profiles.component.html',
+  styleUrls: ['./profiles.component.scss'],
+})
+export class ProfilesComponent {}

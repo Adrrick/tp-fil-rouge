df3c2524dce36ba64ca936f8b59f8c40bf50144f
Hugo Huzé--Cardosi <hugo.cardosi@hotmail.fr>
2023-03-10 10:24:19 +0100

    feat(firebase-user): :sparkles: login / register / logout methods

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from "@angular/fire/compat/firestore";
+import { AngularFirestore, AngularFirestoreCollection } from "@angular/fire/compat/firestore";
 import User from "../../models/User";
+import {Observable} from "rxjs";
+import firebase from "firebase/compat";
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +11,12 @@ export class UserService {
 
   constructor(private afs: AngularFirestore) { }
 
-  createUser(username: string, uid: string) {
+  createUser(username: string, uid: string): Observable<firebase.firestore.DocumentSnapshot<User>> {
     this.afs.collection<User>('/users').doc(uid).set({username, moviesSeen: []}).then();
     return this.getUserByUID(uid);
   }
 
-  getUserByUID(uid: string) {
+  getUserByUID(uid: string): Observable<firebase.firestore.DocumentSnapshot<User>> {
     return this.afs.collection<User>('/users').doc(uid).get();
   }
 }

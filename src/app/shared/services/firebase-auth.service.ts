0770ae6251db1efe946609f18e77179d20352183
Hugo Huzé--Cardosi <hugo.cardosi@hotmail.fr>
2023-03-10 10:24:19 +0100

    feat(firebase-user): :sparkles: login / register / logout methods

--- src/app/shared/services/firebase-auth.service.ts
+++ src/app/shared/services/firebase-auth.service.ts
@@ -1,38 +1,41 @@
 import {Injectable} from '@angular/core';
 import {AngularFireAuth} from "@angular/fire/compat/auth";
+import {UserService} from "./user.service";
+import {StorageService} from "./storage.service";
 
 @Injectable({
   providedIn: 'root',
 })
 export class FirebaseAuthService {
 
-  constructor(private readonly afAuth: AngularFireAuth) { }
+  constructor(private readonly afAuth: AngularFireAuth, private readonly userServices: UserService,
+              private readonly storageService: StorageService) {
+  }
 
   loginEmail(email: string, password: string) {
-    this.afAuth.signInWithEmailAndPassword(email, password)
-      .then((response) => {
-        console.log({response});
-        // TODO use localStorage for accessToken & uid
-      }).catch((err) => {
-       console.log({err});
-       // TODO login failed handler
-    });
+    this.afAuth.signInWithEmailAndPassword(email, password).then((response) => {
+      if (response?.user?.uid) {
+        this.userServices.getUserByUID(response.user.uid).subscribe((user) => {
+          this.storageService.setLoginData(user.id);
+        });
+      }
+    }, (err) => console.log('Error', err));
   }
 
   logout() {
-    // TODO handle logout
+    this.storageService.clearStorage();
   }
 
-  // TODO
   signUpEmail(email: string, password: string, username: string) {
     this.afAuth.createUserWithEmailAndPassword(email, password)
       .then((response) => {
-        console.log({SignUp: 'SignUp', response});
-        response.user?.updateProfile({displayName: username});
-        //TODO get info
+        if (response.user?.uid) {
+          response?.user?.updateProfile({displayName: username});
+          this.userServices.createUser(username, response.user.uid).subscribe((user) => {
+            this.storageService.setLoginData(user.id);
+          });
+        }
       })
-      .catch((error) => {
-        console.log({error});
-      });
+      .catch((err) => console.log('Error', err));
   }
 }

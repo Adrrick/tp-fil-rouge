29a5148f7a16cd9cca5168c1b57f8788cec7265c
jeremie.tergemina <jeremie.tergemina@egs.school>
2023-03-10 14:33:04 +0100

    style: :lipstick: add ui login/register page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,30 +1,41 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
-import {FirebaseAuthService} from "../../shared/services/firebase-auth.service";
-import {AngularFireDatabase} from "@angular/fire/compat/database";
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FirebaseAuthService } from '../../shared/services/firebase-auth.service';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { RouterModule } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon'
 
 @Component({
   selector: 'tp-fil-rouge-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule, MatIconModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-  providers: [FirebaseAuthService, AngularFireDatabase]
+  providers: [FirebaseAuthService, AngularFireDatabase],
 })
 export class LoginComponent {
-  loginForm: FormGroup;
+  public loginForm: FormGroup;
 
-  constructor(private readonly fb: FormBuilder,
-              private authServices: FirebaseAuthService) {
+  constructor(
+    private readonly fb: FormBuilder,
+    private authServices: FirebaseAuthService
+  ) {
     this.loginForm = fb.group({
       email: [undefined, [Validators.required, Validators.email]],
-      password: [undefined, [Validators.required]]
+      password: [undefined, [Validators.required]],
     });
   }
 
-  submit(): void {
-    this.authServices.loginEmail(this.loginForm.controls['email'].value, this.loginForm.controls['password'].value);
+  public submit(): void {
+    this.authServices.loginEmail(
+      this.loginForm.controls['email'].value,
+      this.loginForm.controls['password'].value
+    );
   }
-
 }
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,36 +1,48 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
-import {FirebaseAuthService} from "../../shared/services/firebase-auth.service";
-import {MatchPasswordDirective} from "../../shared/validators/match-password.directive";
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FirebaseAuthService } from '../../shared/services/firebase-auth.service';
+import { MatchPasswordDirective } from '../../shared/validators/match-password.directive';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'tp-fil-rouge-register',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
-  providers: [MatchPasswordDirective]
+  providers: [MatchPasswordDirective],
 })
 export class RegisterComponent {
   registerForm: FormGroup;
 
-  constructor(private readonly fb: FormBuilder,
-              private authServices: FirebaseAuthService) {
-    this.registerForm = fb.group({
-      username: [undefined, [Validators.required]],
-      email: [undefined, [Validators.required, Validators.email]],
-      password: [undefined, [Validators.required]],
-      confirmedPassword: [undefined, [Validators.required]]
+  constructor(
+    private readonly fb: FormBuilder,
+    private authServices: FirebaseAuthService
+  ) {
+    this.registerForm = fb.group(
+      {
+        username: [undefined, [Validators.required]],
+        email: [undefined, [Validators.required, Validators.email]],
+        password: [undefined, [Validators.required]],
+        confirmedPassword: [undefined, [Validators.required]],
       },
       {
-        validators: [new MatchPasswordDirective()]
-      });
+        validators: [new MatchPasswordDirective()],
+      }
+    );
   }
 
   submit(): void {
-    this.authServices.signUpEmail(this.registerForm.controls['email']?.value,
+    this.authServices.signUpEmail(
+      this.registerForm.controls['email']?.value,
       this.registerForm.controls['password']?.value,
-      this.registerForm.controls['username']?.value);
+      this.registerForm.controls['username']?.value
+    );
   }
 }

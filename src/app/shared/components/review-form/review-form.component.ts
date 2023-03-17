0922929915521c5aa01c22c533265f5ddfd7cbed
Hugo Huzé--Cardosi <hugo.cardosi@hotmail.fr>
2023-03-17 10:36:03 +0100

    feat(review): :sparkles: init review-form

---
+++ src/app/shared/components/review-form/review-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tp-fil-rouge-review-form',
+  templateUrl: './review-form.component.html',
+  styleUrls: ['./review-form.component.css']
+})
+export class ReviewFormComponent {
+
+}

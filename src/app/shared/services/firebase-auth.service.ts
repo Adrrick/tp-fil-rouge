d0aacf94992a2fcaf773fa80ff7cb4062cc413e2
Tristan <adrikgw2@gmail.com>
2023-03-17 09:30:46 +0100

    refactor: :recycle: Added Email field to user

--- src/app/shared/services/firebase-auth.service.ts
+++ src/app/shared/services/firebase-auth.service.ts
@@ -1,32 +1,38 @@
-import {Injectable} from '@angular/core';
-import {AngularFireAuth} from "@angular/fire/compat/auth";
-import {UserService} from "./user.service";
-import {StorageService} from "./storage.service";
-import {Router} from "@angular/router";
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { UserService } from './user.service';
+import { StorageService } from './storage.service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FirebaseAuthService {
-
-  constructor(private readonly afAuth: AngularFireAuth, private readonly userServices: UserService,
-              private readonly storageService: StorageService,
-              private readonly router: Router) {
-  }
+  constructor(
+    private readonly afAuth: AngularFireAuth,
+    private readonly userServices: UserService,
+    private readonly storageService: StorageService,
+    private readonly router: Router
+  ) {}
 
   async loginEmail(email: string, password: string) {
-    return this.afAuth.signInWithEmailAndPassword(email, password).then((response) => {
-      if (response?.user?.uid) {
-        this.userServices.getUserByUID(response.user.uid).subscribe((user) => {
-          if (user?.uid) {
-            this.storageService.setLoginData(user?.uid);
-            this.router.navigate(['/']).then();
-          }
-        });
+    return this.afAuth.signInWithEmailAndPassword(email, password).then(
+      (response) => {
+        if (response?.user?.uid) {
+          this.userServices
+            .getUserByUID(response.user.uid)
+            .subscribe((user) => {
+              if (user?.uid) {
+                this.storageService.setLoginData(user?.uid);
+                this.router.navigate(['/']).then();
+              }
+            });
+        }
+      },
+      (err) => {
+        console.error('Error', err);
       }
-    }, (err) => {
-      console.error('Error', err);
-    });
+    );
   }
 
   logout() {
@@ -34,16 +40,19 @@ export class FirebaseAuthService {
   }
 
   signUpEmail(email: string, password: string, username: string) {
-    this.afAuth.createUserWithEmailAndPassword(email, password)
+    this.afAuth
+      .createUserWithEmailAndPassword(email, password)
       .then((response) => {
         if (response?.user?.uid) {
-          response?.user?.updateProfile({displayName: username}).then();
-          this.userServices.createUser(username, response.user.uid).subscribe((user) => {
-            if (user?.uid) {
-              this.storageService.setLoginData(user?.uid);
-              this.router.navigate(['/']).then();
-            }
-          });
+          response?.user?.updateProfile({ displayName: username }).then();
+          this.userServices
+            .createUser(username, email, response.user.uid)
+            .subscribe((user) => {
+              if (user?.uid) {
+                this.storageService.setLoginData(user?.uid);
+                this.router.navigate(['/']).then();
+              }
+            });
         }
       })
       .catch((err) => console.log('Error', err));

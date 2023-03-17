00dc041b70272d4ca9110100d31ca9979d2088da
IShungite <56173955+IShungite@users.noreply.github.com>
2023-03-17 16:16:57 +0100

    feat: ✨ Add user grade

--- src/app/shared/components/user-line/user-line.component.ts
+++ src/app/shared/components/user-line/user-line.component.ts
@@ -4,12 +4,13 @@ import { RouterLink } from '@angular/router';
 import User from 'src/app/models/User';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { getGradeNamePipe } from '../../pipes/get-grade-name/get-grade-name';
 
 
 @Component({
   selector: 'tp-fil-rouge-user-line',
   standalone: true,
-  imports: [CommonModule, RouterLink, MatIconModule, MatButtonModule],
+  imports: [CommonModule, RouterLink, MatIconModule, MatButtonModule, getGradeNamePipe],
   templateUrl: './user-line.component.html',
   styleUrls: ['./user-line.component.scss'],
 })

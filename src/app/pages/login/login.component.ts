f76656845586ac0686eb1676ac7ed0e99a4ce7d9
Adrrick <61594247+Adrrick@users.noreply.github.com>
2023-03-17 16:52:12 +0100

    Revert "#UI auth"

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -4,26 +4,24 @@ import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/
 import {FirebaseAuthService} from "../../shared/services/firebase-auth.service";
 import {AngularFireDatabase} from "@angular/fire/compat/database";
 import {StorageService} from "../../shared/services/storage.service";
-import { RouterModule } from '@angular/router';
-import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'tp-fil-rouge-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, RouterModule, MatIconModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-  providers: [FirebaseAuthService, AngularFireDatabase],
+  providers: [FirebaseAuthService, AngularFireDatabase]
 })
 export class LoginComponent {
-  public loginForm: FormGroup;
+  loginForm: FormGroup;
 
   constructor(private readonly fb: FormBuilder,
               private authServices: FirebaseAuthService,
               private storageServices: StorageService ) {
     this.loginForm = fb.group({
       email: [undefined, [Validators.required, Validators.email]],
-      password: [undefined, [Validators.required]],
+      password: [undefined, [Validators.required]]
     });
   }
 
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -4,15 +4,14 @@ import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/
 import {FirebaseAuthService} from "../../shared/services/firebase-auth.service";
 import {MatchPasswordDirective} from "../../shared/validators/match-password.directive";
 import {UserService} from "../../shared/services/user.service";
-import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'tp-fil-rouge-register',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, RouterModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
-  providers: [MatchPasswordDirective],
+  providers: [MatchPasswordDirective]
 })
 export class RegisterComponent {
   registerForm: FormGroup;
@@ -27,9 +26,8 @@ export class RegisterComponent {
         confirmedPassword: [undefined, [Validators.required]]
       },
       {
-        validators: [new MatchPasswordDirective()],
-      }
-    );
+        validators: [new MatchPasswordDirective()]
+      });
   }
 
   async submit(): Promise<void> {

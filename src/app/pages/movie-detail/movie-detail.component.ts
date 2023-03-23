7cc5f0b2d1c40e490d6d1274da69053093ffca3e
Hugo Huzé--Cardosi <hugo.cardosi@hotmail.fr>
2023-03-23 11:54:00 +0100

    feat(review): :construction: creatReviews

--- src/app/pages/movie-detail/movie-detail.component.ts
+++ src/app/pages/movie-detail/movie-detail.component.ts
@@ -2,13 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MoviesService } from '../../shared/services/movies.service';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
 import MovieDetails from 'src/app/models/Movie-details';
+import { Observable } from "rxjs";
+import {ReviewFormComponent} from "../../shared/components/review-form/review-form.component";
+
 
 @Component({
   selector: 'tp-fil-rouge-movie-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReviewFormComponent],
   templateUrl: './movie-detail.component.html',
   styleUrls: ['./movie-detail.component.scss'],
   providers: [MoviesService],

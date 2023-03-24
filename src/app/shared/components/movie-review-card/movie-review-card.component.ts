ddb0bbd6e8e00d37fcfc5dec5068d92b0e3dd221
Adrrick <adrikgw2@gmail.com>
2023-03-24 12:59:54 +0100

    style: :art: Added style and redirection to reviewCard

--- src/app/shared/components/movie-review-card/movie-review-card.component.ts
+++ src/app/shared/components/movie-review-card/movie-review-card.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import Review from 'src/app/models/Review';
 import { MatCardModule } from '@angular/material/card';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'tp-fil-rouge-movie-review-card',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule, MatCardModule, RouterLink],
   templateUrl: './movie-review-card.component.html',
   styleUrls: ['./movie-review-card.component.scss'],
 })

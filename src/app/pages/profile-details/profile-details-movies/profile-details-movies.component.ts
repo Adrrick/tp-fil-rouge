5989556ac4971bb26ad9b57641547ed953274ecc
Tristan <adrikgw2@gmail.com>
2023-03-24 12:53:46 +0100

    feat: :sparkles: Pass user as props in details

--- src/app/pages/profile-details/profile-details-movies/profile-details-movies.component.ts
+++ src/app/pages/profile-details/profile-details-movies/profile-details-movies.component.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+import { MoviesService } from 'src/app/shared/services/movies.service';
+import { ActivatedRoute } from '@angular/router';
+import User from 'src/app/models/User';
+import { UserService } from 'src/app/shared/services/user.service';
+import MovieDetails from 'src/app/models/Movie-details';
 
 @Component({
   selector: 'tp-fil-rouge-profile-details-movies',
@@ -8,4 +14,19 @@ import { CommonModule } from '@angular/common';
   templateUrl: './profile-details-movies.component.html',
   styleUrls: ['./profile-details-movies.component.scss'],
 })
-export class ProfileDetailsMoviesComponent {}
+export class ProfileDetailsMoviesComponent implements OnInit {
+  @Input() user?: User;
+  viewedMovies$?: Observable<MovieDetails[]>;
+
+  constructor(
+    private route: ActivatedRoute,
+    private movieService: MoviesService,
+    private usersService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    if (this.user) {
+      console.log(this.user);
+    }
+  }
+}
--- src/app/pages/profile-details/profile-details-reviews/profile-details-reviews.component.ts
+++ src/app/pages/profile-details/profile-details-reviews/profile-details-reviews.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import User from 'src/app/models/User';
+import { UserService } from 'src/app/shared/services/user.service';
 
 @Component({
   selector: 'tp-fil-rouge-profile-details-reviews',
@@ -8,4 +11,17 @@ import { CommonModule } from '@angular/common';
   templateUrl: './profile-details-reviews.component.html',
   styleUrls: ['./profile-details-reviews.component.scss'],
 })
-export class ProfileDetailsReviewsComponent {}
+export class ProfileDetailsReviewsComponent implements OnInit {
+  @Input() user?: User;
+
+  constructor(
+    private route: ActivatedRoute,
+    private usersService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    if (this.user) {
+      console.log(this.user);
+    }
+  }
+}

675e844a177444bb2d2a7951a26c5af7a8c696b5
Hugo Huzé--Cardosi <hugo.cardosi@hotmail.fr>
2023-03-24 12:52:06 +0100

    feat(review): :construction: star rating for review

--- src/app/shared/components/review-form/review-form.component.ts
+++ src/app/shared/components/review-form/review-form.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ReviewService} from "../../services/review.service";
 
 @Component({
   selector: 'tp-fil-rouge-review-form',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./review-form.component.css']
 })
 export class ReviewFormComponent {
+  reviewForm: FormGroup;
 
+  constructor(private readonly fb: FormBuilder, private readonly reviewService: ReviewService) {
+    this.reviewForm = this.fb.group({
+      title: [undefined, [Validators.required, Validators.minLength(4)]],
+      rating: [undefined, [Validators.required, Validators.min(0), Validators.max(5)]],
+      comment: ['']
+    });
+  }
+
+
+  submit() {
+    console.log(this.reviewForm);
+  }
 }

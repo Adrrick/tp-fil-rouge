3605d302d93c4198c0f1031097d784e3de3c3234
Adrrick <adrikgw2@gmail.com>
2023-03-24 12:59:53 +0100

    feat: :sparkles: Display reviews

--- src/app/pages/profile-details/profile-details-reviews/profile-details-reviews.component.ts
+++ src/app/pages/profile-details/profile-details-reviews/profile-details-reviews.component.ts
@@ -1,21 +1,18 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
-import { UserService } from 'src/app/shared/services/user.service';
 import Review from 'src/app/models/Review';
+import { MovieReviewCardComponent } from 'src/app/shared/components/movie-review-card/movie-review-card.component';
 
 @Component({
   selector: 'tp-fil-rouge-profile-details-reviews',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MovieReviewCardComponent],
   templateUrl: './profile-details-reviews.component.html',
   styleUrls: ['./profile-details-reviews.component.scss'],
 })
-export class ProfileDetailsReviewsComponent{
+export class ProfileDetailsReviewsComponent {
   @Input() reviews?: Review[];
 
-  constructor(
-    private route: ActivatedRoute,
-    private usersService: UserService
-  ) {}
+  constructor(private route: ActivatedRoute) { }
 }
